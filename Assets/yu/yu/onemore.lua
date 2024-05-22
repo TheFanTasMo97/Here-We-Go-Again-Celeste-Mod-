5f4f5030520fedccd5d0f3ef937f1d9c9d40e63e
TheFanTasMo97 <44678268+TheFanTasMo97@users.noreply.github.com>
2024-05-23 01:41:43 +0200

    0.0.3 & pre 0.0.4
    - Add custom background music
    - A block appeared after the onemore cutscene triggered for the first time on the room
    - Change the trigger area preposition for the onemore cutscene
    - Modify a cutscene in the last room to prevent player to fall
    - Modify height falling block on the last room
    - Fix onemore cinematics where’s is playing always after a retry
    - Fix postcard
    
    - Modify some background wall

--- Assets/yu/yu/onemore.lua
+++ Assets/yu/yu/onemore.lua
@@ -1,14 +1,21 @@
 local lua_helper = celeste.Mod.LuaCutscenes.LuaHelper
 
+
 function onBegin()
-	disableMovement()
-	disablePause()
-	say("1_TEST_R01_05_DIALOG_2")
-	setFlag("retrying_tower", true)
-	instantTeleportTo(150, -400)
+	local alreadHadCinematics = getFlag("alreadyHadCinematicsOneMore")
+	if not alreadHadCinematics then
+		disableMovement()
+		disablePause()
+		player.DummyGravity = false
+		say("1_TEST_R01_05_DIALOG_2")
+		setFlag("retrying_tower", true)
+		instantTeleportTo(150, -400)
+	end
 end
 
 function onEnd()
 	enableMovement()
 	enablePause()
+	player.DummyGravity = true
+	setFlag("alreadyHadCinematicsOneMore", true)
 end
